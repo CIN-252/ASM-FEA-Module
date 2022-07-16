a027d3919d8d4c977e8744cbcf3e9cf8b865fd86
CIN-252 <thuanle.25296@gmail.com>
2022-07-16 22:54:30 +0700

    Change name file

---
+++ TinhTienPhongKS/index.js
@@ -0,0 +1,8 @@
+const checkIn = new Date("2022-07-15 10:00:00");
+const checkOut = new Date("2022-07-16 10:00:00");
+
+const ngayVao = checkIn.getDate();
+const ngayRa = checkOut.getDate();
+
+const gioVao = checkIn.getHours();
+const gioRa = checkOut.getHours();
