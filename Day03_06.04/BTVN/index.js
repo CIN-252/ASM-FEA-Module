63cd77820a8033e4b5b2f0264511ae82eafe8277
CIN-252 <thuanle.25296@gmail.com>
2022-07-20 21:24:48 +0700

    update 20.07

---
+++ Day03_06.04/BTVN/index.js
@@ -0,0 +1,89 @@
+// Input a = [1,2,3,4] ; Output: [1,4,3,8] (Gợi ý: số chẵn nhân 2). Thực hiện với hàm Map:
+const numbers = [1, 2, 3, 4];
+
+function doubleEvenNumber(number) {
+  return number.map((number) => {
+    if (number % 2 == 0) return (number *= 2);
+    else return number;
+  });
+}
+
+console.log("Result: ", doubleEvenNumber(numbers));
+
+// Thực hiện với hàm Filter:
+const animals = [
+  {
+    name: "cat",
+    size: "small",
+    weight: 5,
+  },
+  {
+    name: "dog",
+    size: "medium",
+    weight: 10,
+  },
+  {
+    name: "elephant",
+    size: "big",
+    weight: 5000,
+  },
+];
+
+// output: (mảng các con vật có weight > 50 kg):
+function filterAnimalsOver50Kg(animal) {
+  return animal.filter((animal) => {
+    if (animal.weight > 50) return animal;
+  });
+}
+console.log("Animal over 50kg: ", filterAnimalsOver50Kg(animals));
+
+// output: (mảng các con vật có chữ t trong name):
+function filterAnimalsContainTInName(animal) {
+  return animal.filter((animal) => {
+    if (animal.name.includes("t")) return animal;
+  });
+}
+console.log(
+  'Animal contain "t" in name: ',
+  filterAnimalsContainTInName(animals)
+);
+
+// output: apply filter and map [ "name: cat - size: small - weight: 5kg", "name: elephant - size: big - weight: 5000kg"]
+function filterAnimalsByName(animal) {
+  return animal.filter((animal) => {
+    if (animal.name == "cat" || animal.name == "elephant") return animal;
+  });
+}
+const result = filterAnimalsByName(animals).map((animal) => {
+  return `"name: ${animal.name} - size: ${animal.size} - weight: ${animal.weight}kg"`;
+});
+console.log(result);
+
+// TÍnh tổng mảng [1,2,3,4] với dkien: nếu giá trị là số chẵn thì x2
+// Input: [1,2,3,4]
+// Ouput : 16
+
+const numbersInput = [1, 2, 3, 4];
+
+function sumArrWithDoubleEvenNumber(number) {
+  return doubleEvenNumber(number).reduce((accumulate, currValue) => {
+    return accumulate + currValue;
+  });
+}
+console.log("SUM: ", sumArrWithDoubleEvenNumber(numbersInput));
+
+// Tính tổng mảng [1,2,3,4] với điều kiện số lẻ * 2, số chẵn / 2
+// Input: [1,2,3,4]
+// Ouput : 11
+function doubleOddAndHalveEvenNumber(number) {
+  return number.map((number) => {
+    if (number % 2 == 0) return (number /= 2);
+    else if (number % 2 != 0) return (number *= 2);
+  });
+}
+function sumArrWithDoubleOddAndHalveEvenNumber(number) {
+  return doubleOddAndHalveEvenNumber(number).reduce((accumulate, currValue) => {
+    return accumulate + currValue;
+  });
+}
+console.log("SUM: ", sumArrWithDoubleOddAndHalveEvenNumber(numbersInput));
--- Day03_06.04/index.js
+++ Day03_06.04/index.js
@@ -1,5 +1,9 @@
+// Map:
+// BT1:
+console.log("BT1:");
 // arrInput = [1, 2, 3, 4]
 // arrOutput = [6, 7, 8, 9]
+
 const arrInput = [1, 2, 3, 4];
 const arrOutput = arrInput.map(plusFiveToEachElement);
 
@@ -7,9 +11,15 @@ function plusFiveToEachElement(value) {
   return value + 5;
 }
 
-console.log(arrOutput);
+const arrOutput_Callback = arrInput.map((value) => value + 5);
 
+console.log("Result: ", arrOutput);
+console.log("Result: ", arrOutput_Callback);
+
+// BT2:
+console.log("BT2:");
 /* arrOutput: [{value: 1, isOdd: true}, {value: 2, isOdd: false}, {value: 3, isOdd: true}, {value: 4, isOdd: false}] */
+
 function isOddNumber(number) {
   if (number % 2 == 1) return true;
   return false;
@@ -17,15 +27,82 @@ function isOddNumber(number) {
 
 function convertToCheckedOddArr(arr) {
   return arr.map((number) => {
-    // let value = number;
     let isOdd;
     if (isOddNumber(number)) {
       isOdd = "true";
     } else {
       isOdd = "false";
     }
-    return { number, isOdd };
+    return { number: number, isOdd: isOdd };
   });
 }
 
-console.log(convertToCheckedOddArr(arrInput));
+console.log("Result: ", convertToCheckedOddArr(arrInput));
+
+/* BT3:
+Output: ["name:... - size:... - weight:...kg", "...", "..."] */
+console.log("BT3:");
+
+const animals = [
+  {
+    name: "cat",
+    size: "small",
+    weight: 5,
+  },
+  {
+    name: "dog",
+    size: "medium",
+    weight: 10,
+  },
+  {
+    name: "elephant",
+    size: "big",
+    weight: 5000,
+  },
+];
+
+function getAnimalInfo(animal) {
+  return `name: ${animal.name} - size: ${animal.size} - weight: ${animal.weight}kg`;
+}
+
+function convertAnimalToString() {
+  return animals.map(getAnimalInfo);
+}
+
+const convertByCallback = animals.map(
+  (animal) =>
+    `name: ${animal.name} - size: ${animal.size} - weight: ${animal.weight}kg`
+);
+
+console.log(animals);
+console.log("Result: ", convertAnimalToString());
+console.log("Result: ", convertByCallback);
+
+// BT4: Filter
+// Filter 4 Character:
+console.log("BT4");
+const words = ["Thuan", "Cin", "An", "Nguyen", "Loan", "Ngan"];
+const _4Character = words.filter((word) => word.length === 4);
+console.log("4 Character: ", _4Character);
+
+// Contain "a":
+function findACharacterInName() {
+  return words.filter((word) => {
+    return word.includes("a");
+  });
+}
+
+console.log('Contain "a": ', findACharacterInName());
+
+// const AContain = words.filter((word) => word.includes("a"));
+// console.log('Contain "a": ', AContain);
+
+// BT5: Reduce
+console.log("BT5");
+const input = [1, 2, 3, 4];
+
+const sum = input.reduce((accumulate, currValue, currIndex, array) => {
+  return accumulate + currValue;
+}, 0);
+
+console.log("Sum: ", sum);
